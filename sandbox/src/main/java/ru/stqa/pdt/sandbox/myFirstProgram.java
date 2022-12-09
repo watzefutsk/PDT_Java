4115252bdb83a2c58fd57e45ba868c4206356052
Dmitriy <pdd.javadeveloper@yandex.ru>
2022-12-09 21:26:15 +0300

    Some functions added

--- sandbox/src/main/java/ru/stqa/pdt/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pdt/sandbox/myFirstProgram.java
@@ -2,6 +2,24 @@ package ru.stqa.pdt.sandbox;
 
 public class myFirstProgram {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		hello("Dima");
+		double v = 5;
+		System.out.println("Площадь квадрата со стороной " + v + " равна " + area(v));
+
+		double a = 7;
+		double b = 8;
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a, b));
+	}
+
+	public static void hello(String somestr) {
+		System.out.println("Hello, " + somestr + "!");
+	}
+
+	public static double area (double l) {
+		return l * l;
+	}
+
+	public static double area (double a, double b) {
+		return a * b;
 	}
 }
