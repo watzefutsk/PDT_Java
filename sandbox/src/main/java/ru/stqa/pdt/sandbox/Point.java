9de6e9a0107c9fbb3ce637010d9f58493c2dba3d
Dmitriy <pdd.javadeveloper@yandex.ru>
2022-12-10 11:13:46 +0300

    Задание №2. Добавлен конструктор

--- sandbox/src/main/java/ru/stqa/pdt/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pdt/sandbox/Point.java
@@ -3,4 +3,9 @@ package ru.stqa.pdt.sandbox;
 public class Point {
     public double p1;
     public double p2;
+
+    public Point (double p1, double p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
 }
--- sandbox/src/main/java/ru/stqa/pdt/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pdt/sandbox/myFirstProgram.java
@@ -3,13 +3,8 @@ package ru.stqa.pdt.sandbox;
 public class myFirstProgram {
 	public static void main(String[] args) {
 
-		Point p1 = new Point();
-		p1.p1 = 2;
-		p1.p2 = 4;
-
-		Point p2 = new Point();
-		p2.p1 = 4;
-		p2.p2 = 6;
+		Point p1 = new Point(2, 4);
+		Point p2 = new Point(4,6);
 
 		System.out.println("Расстояние между точками: " + distance(p1, p2));
 
