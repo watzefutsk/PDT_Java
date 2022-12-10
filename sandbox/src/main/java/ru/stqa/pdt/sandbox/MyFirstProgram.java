70e69f778750d208b95d40f126bb6015c22f1d1a
Dmitriy <pdd.javadeveloper@yandex.ru>
2022-12-10 21:34:46 +0300

    Задание №3. Решено

--- sandbox/src/main/java/ru/stqa/pdt/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pdt/sandbox/MyFirstProgram.java
@@ -3,8 +3,8 @@ package ru.stqa.pdt.sandbox;
 public class MyFirstProgram {
 	public static void main(String[] args) {
 
-		Point p1 = new Point(2, 4);
-		Point p2 = new Point(7, 1);
+		Point p1 = new Point(-0.01,-1);
+		Point p2 = new Point(-100, -5);
 
 		System.out.println("Расстояние между точками А и В равно " + p1.distance(p2));
 	}
--- sandbox/src/test/java/ru/stqa/pdt/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pdt/sandbox/PointTest.java
@@ -5,10 +5,22 @@ import org.testng.annotations.Test;
 
 public class PointTest {
     @Test
-    public void testDistance() {
+    public void positivePoints() {
         Point p1 = new Point(2,4);
         Point p2 = new Point(7,1);
         Assert.assertEquals(p1.distance(p2), 6.324555320336759);
+    }
+    @Test
+    public void nullPoints() {
+        Point p1 = new Point(0,0);
+        Point p2 = new Point(0,0);
+        Assert.assertEquals(p1.distance(p2),0);
+    }
+    @Test
+    public void negativePoints() {
+        Point p1 = new Point(-0.01,-1);
+        Point p2 = new Point(-100, -5);
+        Assert.assertEquals(p1.distance(p2),95.00515828101125);
     }
 
 }
