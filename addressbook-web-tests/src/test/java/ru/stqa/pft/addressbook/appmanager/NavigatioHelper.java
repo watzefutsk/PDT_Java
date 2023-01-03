1ad40f2df57d3f38b4ef24483306606eabdc56b3
Dmitriy <pdd.javadeveloper@yandex.ru>
2023-01-03 18:20:22 +0300

    Transitions between pages optimized

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigatioHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigatioHelper.java
@@ -10,10 +10,16 @@ public class NavigatioHelper extends HelperBase {
         super(wd);
     }
     public void goToGroupPage() {
+        if (isElementPresent(By.tagName("h1")) && wd.findElement(By.tagName("h1")).getText().equals("Groups") && isElementPresent(By.name("new"))) {
+            return;
+        }
         click(By.linkText("groups"));
     }
 
     public void goToContactCreationPage() {
+        if (isElementPresent(By.tagName("h1")) && wd.findElement(By.tagName("h1")).getText().equals("Edit / add address book entry") && isElementPresent(By.name("submit"))) {
+            return;
+        }
       wd.findElement(By.linkText("add new")).click();
     }
 }
