c9e4aaa5d05d2838b31d1dc42028e3c62c2e7d45
Dmitriy <pdd.javadeveloper@yandex.ru>
2023-01-04 19:59:55 +0300

    Selection of the last group in list added

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -9,11 +9,11 @@ public class GroupModificationTest extends TestBase {
     @Test
     public void testGroupModification() {
         app.getNavigatioHelper().goToGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
         if (!app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
-        app.getGroupHelper().selectGroup();
+        int before = app.getGroupHelper().getGroupCount();
+        app.getGroupHelper().selectGroup(before - 1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test1", "test1", "test2"));
         app.getGroupHelper().submitGroupModification();
