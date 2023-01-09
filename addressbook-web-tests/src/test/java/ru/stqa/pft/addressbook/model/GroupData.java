8715db8ebb6bbd2714960a7c70a6a8684c357006
Dmitriy <pdd.javadeveloper@yandex.ru>
2023-01-09 18:24:18 +0300

    Добавлена проверка по сравнению списка групп после модификации

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -3,16 +3,27 @@ package ru.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
+    private final String id;
     private final String name;
     private final String header;
     private final String footer;
 
     public GroupData(String name, String header, String footer) {
+        this.id = null;
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
+    public GroupData(String id, String name, String header, String footer) {
+        this.id = id;
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
 
+    public String getId() {
+        return id;
+    }
     public String getName() {
         return name;
     }
@@ -32,18 +43,22 @@ public class GroupData {
 
         GroupData groupData = (GroupData) o;
 
+        if (!Objects.equals(id, groupData.id)) return false;
         return Objects.equals(name, groupData.name);
     }
 
     @Override
     public int hashCode() {
-        return name != null ? name.hashCode() : 0;
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
     }
 
     @Override
     public String toString() {
         return "GroupData{" +
-                "name='" + name + '\'' +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
                 '}';
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTest extends TestBase {
@@ -17,10 +18,14 @@ public class GroupModificationTest extends TestBase {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test1", "test2"));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(),  "test1", "test1", "test2");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
     }
 }
